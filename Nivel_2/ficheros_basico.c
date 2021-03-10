4f6c5804335433c627329e5d3b45900dd7bee4b1
Jordism <jordi.sevilla1@estudiant.uib.cat>
2021-03-10 19:28:06 +0100

    v0.1 (Preparación y declaración de funciones)

---
+++ Nivel_2/ficheros_basico.c
@@ -0,0 +1,31 @@
+#include "ficheros_basico.h"
+
+int tamMB(unsigned int nbloques){
+    
+    return 0;
+
+}
+
+int tamAI(unsigned int inodos){
+    
+    return 0;
+
+}
+
+int initSB(unsigned int nbloques, unsigned int ninodos){
+    
+    return 0;
+
+}
+
+int initMB(){
+    
+    return 0;
+
+}
+
+int initAI(){
+
+    return 0;
+
+}
